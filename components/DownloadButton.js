b37a3cdf3a6f2d5123848c9780edaede4dc192a6
Harsh2313 <kaushikh464@gmail.com>
2024-09-16 20:27:36 +0530

    saved

--- components/DownloadButton.js
+++ components/DownloadButton.js
@@ -24,26 +24,7 @@ const DownloadButton = () => {
       });
     }, 500);
   };
-  useEffect(() => {
-    // Create a script element
-    const script = document.createElement("script");
-    script.src = "https://code.jivosite.com/widget/hLljwWDcZ2"; // Replace with your actual JivoChat script URL
-    script.async = true;
-    script.onload = () => {
-      console.log("JivoChat script loaded successfully");
-    };
-    script.onerror = () => {
-      console.error("Failed to load JivoChat script");
-    };
-    // Append the script to the body
-    document.body.appendChild(script);
-
-    // Clean up script on component unmount
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
+  
   return (
     <>
       <Button variant="primary" onClick={handleDownload}>
