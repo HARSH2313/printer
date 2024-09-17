3f3ef26bacc3f204105ddb72c2f915a444f963fc
Harsh2313 <kaushikh464@gmail.com>
2024-09-17 10:02:11 +0530

    saved

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,22 @@ import "../styles/globals.css";
 import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
-  
+  useEffect(()=>{
+    const script = document.createElement("script");
+    script.src = "https://code.jivochat.com/widget/hL1jwWDcZ2";
+    script.async = true;
+    script.onload=()=>{
+      console.log("jivochat script loaded successfully");
+    };
+    script.onerror=()=>{
+      console.error("Fail to load jivochat script ");
+    };
+    document.body.appendChild(script);
+    return ()=>{
+      document.body.removeChild(script);
+    }
+
+  },[]);
   return (
     <html lang="en">
       <body>
